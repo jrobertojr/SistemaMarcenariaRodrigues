740ded7177c8fcc4043660275b0cdd2c296597c3
Beto <j.roberto_jr@hotmail.com>
2020-09-01 11:33:52 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaMarcenariaRodrigues/Conexao/Conn.cs
@@ -0,0 +1,136 @@
+﻿using MySql.Data.MySqlClient;
+using SistemaMarcenariaRodrigues.Log;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace SistemaMarcenariaRodrigues.Conexao
+{
+    class Conn
+    {
+
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Contrutor
+        public Conn()
+        {
+            Inicializar();
+        }
+
+        private void Inicializar()
+        {
+            try
+            {
+                string connectionString;
+
+                server = "";
+                database = "";
+                uid = "";
+                password = "";
+                connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+                connection = new MySqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro de conexão, contate o Administrador");
+                RegistraLog.Log($"Erro ao gerar string de conexão --- {ex}");
+            }
+
+        }
+
+        //Abrir Conexão
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Não foi possível se conectar ao banco de dados.  Contate o administrador");
+                        RegistraLog.Log($"Erro ao conecatr com o banco de dados --- {ex}");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Usuário e/ou senha inválido, por favor tente novamente");
+                        RegistraLog.Log($"Erro 1045 --- {ex}");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Fechar Conexão
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Erro de Conexão");
+                RegistraLog.Log($"Erro ao fechar conexão --- {ex}");
+                return false;
+            }
+        }
+
+        //Metodo para ação ativa
+        public void Sql(String SQLq)
+        {
+            try
+            {
+                string query = SQLq;
+
+                if (this.OpenConnection() == true)
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.ExecuteNonQuery();
+                    this.CloseConnection();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Impossível estabelecer conexão");
+                RegistraLog.Log($"Erro ao executar comando MySQL --- Query: {SQLq} --- {ex}");
+            }
+
+        }
+
+        //Metodo para ação passiva
+        public DataTable SqlDataTable(String SQLq)
+        {
+            string query = SQLq;
+            DataTable dTable = new DataTable();
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandTimeout = 99999;
+
+                MySqlDataAdapter adap = new MySqlDataAdapter(query, connection);
+                adap.Fill(dTable);
+
+                this.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Impossível estabelecer conexão");
+                RegistraLog.Log($"Erro ao retornar dados para o DataTable --- Query: {SQLq} --- {ex}");
+            }
+            return dTable;
+        }
+
+    }
+}
---
+++ SistemaMarcenariaRodrigues/Main.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SistemaMarcenariaRodrigues
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
