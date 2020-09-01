135262951a4ec1e365717e0032834d09ad1a4ad5
Beto <j.roberto_jr@hotmail.com>
2020-09-01 11:40:53 -0300

    Adicionado controle Log de erros

---
+++ SistemaMarcenariaRodrigues/Log/RegistraLog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace SistemaMarcenariaRodrigues.Log
+{
+    class RegistraLog
+    {
+        private static string caminhoExe = string.Empty;
+
+        //Cria o arquivo Log
+        public static bool Log(string strMensagem, string strNomeArquivo = "Log")
+        {
+            try
+            {
+                caminhoExe = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string caminhoArquivo = Path.Combine(caminhoExe, strNomeArquivo + " " + DateTime.Now.ToString("dd-MM-yyyy"));
+                if (!File.Exists(caminhoArquivo))
+                {
+                    FileStream arquivo = File.Create(caminhoArquivo);
+                    arquivo.Close();
+                }
+                using (StreamWriter w = File.AppendText(caminhoArquivo))
+                {
+                    AppendLog(strMensagem, w);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        //Escreve no arquivo Log
+        private static void AppendLog(string logMensagem, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.Write("\r\nLog Entrada : ");
+                txtWriter.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+                txtWriter.WriteLine("  :");
+                txtWriter.WriteLine($"  :{logMensagem}");
+                txtWriter.WriteLine("------------------------------------");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
