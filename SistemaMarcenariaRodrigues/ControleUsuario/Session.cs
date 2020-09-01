1e5f17ae5e10883020b2260bf56b8e35948310b3
Beto <j.roberto_jr@hotmail.com>
2020-09-01 13:10:06 -0300

    Adicionado classes para criptografia e controle de usuario

---
+++ SistemaMarcenariaRodrigues/ControleUsuario/Session.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMarcenariaRodrigues.ControleUsuario
+{
+    public sealed class Session
+    {
+
+        private static volatile Session instance;
+        private static object sync = new Object();
+
+        private Session() { }
+
+        public static Session Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (sync)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Session();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Propriedade para o ID do usuario
+        /// </summary>
+        public int UserID { get; set; }
+
+    }
+}
---
+++ SistemaMarcenariaRodrigues/Criptografia/HashMd5.cs
@@ -0,0 +1,36 @@
+﻿using SistemaMarcenariaRodrigues.Log;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SistemaMarcenariaRodrigues.Criptografia
+{
+    class HashMd5
+    {
+        public static string GerarMd5(string input)
+        {
+            try
+            {
+                MD5 md5Hash = MD5.Create();
+                // Converter a String para array de bytes, que é como a biblioteca trabalha.
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Cria-se um StringBuilder para recompôr a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop para formatar cada byte como uma String em hexadecimal
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                return sBuilder.ToString();
+            }
+            catch (Exception ex)
+            {
+                RegistraLog.Log("Erro ao criptografar" + ex);
+                return null;
+            }
+        }
+    }
+}
